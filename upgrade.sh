3ceaceadf01b8be828f98e9a6f2434c84b20b25b
demesm <demesm@outlook.com>
2018-06-14 01:15:06 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -9,12 +9,12 @@ print_status() {
 }
 
 wget https://github.com/AegeusCoin/aegeus/releases/download/1.2/Aegeus-1.2-Linux-64-bit.tar.gz && tar -zxvf Aegeus-1.2-Linux-64-bit.tar.gz
-systemctl stop Aegeus.service
-mv /root/Aegeus-1.2-Linux-64-bit/aegeus-cli /usr/local/bin/aegeus-cli
-mv /root/Aegeus-1.2-Linux-64-bit/aegeusd /usr/local/bin/aegeusd
+systemctl stop Aegeus.service && mv /root/Aegeus-1.2-Linux-64-bit/aegeus-cli /usr/local/bin/aegeus-cli && mv /root/Aegeus-1.2-Linux-64-bit/aegeusd /usr/local/bin/aegeusd
 
 print_status "start alias in wallet, then hit enter to continue"
 pause
 
-systemctl start Aegeus.service
-systemctl status Aegeus.service
+sleep 10
+
+systemctl restart Aegeus.service
+watch -n2 aegeus-cli masternode status
